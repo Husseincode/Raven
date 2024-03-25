bd8f79363afec25784bfb79fc6a822837676bb4f
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2024-03-25 21:16:40 +0100

    raven application

--- raven_application/src/RavenNav.js
+++ raven_application/src/RavenNav.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 const RavenNav = () => {
     return (
-        <Router>
+        <Router basename='/home'>
             <Routes>
                 <Route Component={RavenApp} path='/home' exact/>
                 <Route Component={RavenMiniCalculator} path='/calculator'/>
